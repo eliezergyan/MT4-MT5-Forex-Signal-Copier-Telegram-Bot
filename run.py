7f05d30f9189eb573393feba270796d320101fe5
eliezergyan <eliezer.gyan@amalitech.com>
2023-04-15 05:35:22 +0000

    Removed username check

--- run.py
+++ run.py
@@ -485,9 +485,9 @@ def unknown_command(update: Update, context: CallbackContext) -> None:
         update: update from Telegram
         context: CallbackContext object that stores commonly used objects in handler callbacks
     """
-    if(not(update.effective_message.chat.username == TELEGRAM_USER)):
-        update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
-        return
+    # if(not(update.effective_message.chat.username == TELEGRAM_USER)):
+    #     update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
+    #     return
 
     update.effective_message.reply_text("Unknown command. Use /trade to place a trade or /calculate to find information for a trade. You can also use the /help command to view instructions for this bot.")
 
@@ -566,9 +566,9 @@ def Trade_Command(update: Update, context: CallbackContext) -> int:
         update: update from Telegram
         context: CallbackContext object that stores commonly used objects in handler callbacks
     """
-    if(not(update.effective_message.chat.username == TELEGRAM_USER)):
-        update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
-        return ConversationHandler.END
+    # if(not(update.effective_message.chat.username == TELEGRAM_USER)):
+    #     update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
+    #     return ConversationHandler.END
     
     # initializes the user's trade as empty prior to input and parsing
     context.user_data['trade'] = None
@@ -585,9 +585,9 @@ def Calculation_Command(update: Update, context: CallbackContext) -> int:
         update: update from Telegram
         context: CallbackContext object that stores commonly used objects in handler callbacks
     """
-    if(not(update.effective_message.chat.username == TELEGRAM_USER)):
-        update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
-        return ConversationHandler.END
+    # if(not(update.effective_message.chat.username == TELEGRAM_USER)):
+    #     update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
+    #     return ConversationHandler.END
 
     # initializes the user's trade as empty prior to input and parsing
     context.user_data['trade'] = None
